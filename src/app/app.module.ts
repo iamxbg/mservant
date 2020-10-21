fa8dd456979c5eeb30d0a85846fe96862a391a6e
wilson.goodfellow <heimoheimo@126.com>
2020-10-21 21:30:06 +0800

    add the top,left,right position structure components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeftComponent } from './left/left.component';
+import { RightComponent } from './right/right.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftComponent,
+    RightComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
